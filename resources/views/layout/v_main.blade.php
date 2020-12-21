009c905dbbf14567a7ee727e5b443ff4ac4c390c
MNasirulUmam <nasirul250400@gmail.com>
2020-12-21 15:32:24 +0700

    megubah file V_main dan V_index

--- resources/views/layout/v_main.blade.php
+++ resources/views/layout/v_main.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <title>@yield('title')</title>
@@ -46,7 +47,7 @@
         <div class="social-links">
           <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
           <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-          <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+          <a href="https://wa.me/6282330263968" class="whatsapp"><i class="fa fa-whatsapp"></i></a>
           <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
         </div>
       </div>
@@ -56,7 +57,7 @@
 
       <div class="logo float-left">
         <!-- Uncomment below if you prefer to use an image logo -->
-        <h1 class="text-light"><a href="#intro" class="scrollto"><span>Rapid</span></a></h1>
+        <h1 class="text-light"><a href="#intro" class="scrollto"><span>E-LAUNDRY</span></a></h1>
         <!-- <a href="#header" class="scrollto"><img src="img/logo.png" alt="" class="img-fluid"></a> -->
       </div>
 
@@ -87,7 +88,7 @@
           <li><a href="#footer">Contact Us</a></li>
         </ul>
       </nav><!-- .main-nav -->
-      
+
     </div>
   </header><!-- #header -->
 
@@ -108,54 +109,54 @@
 
             <div class="row">
 
-                <div class="col-sm-6">
+              <div class="col-sm-6">
 
-                  <div class="footer-info">
-                    <h3>Rapid</h3>
-                    <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus. Scelerisque felis imperdiet proin fermentum leo. Amet volutpat consequat mauris nunc congue.</p>
-                  </div>
-
-                  <div class="footer-newsletter">
-                    <h4>Our Newsletter</h4>
-                    <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna veniam enim veniam illum dolore legam minim quorum culpa amet magna export quem.</p>
-                    <form action="" method="post">
-                      <input type="email" name="email"><input type="submit"  value="Subscribe">
-                    </form>
-                  </div>
-
+                <div class="footer-info">
+                  <h3>E-Laudry</h3>
+                  <p>9/10 pelanggan laundry pernah kehilangan pakaian pada layanan laundry? jangan tunggu sampai pakaian kesayangan Anda hilang, tertukar atau rusak! Dengan layanan kami, Anda bisa tenang dan santai sepenuhnya!<br>Pesan sekarang dan kami akan melayani Anda sekarang juga!</p>
                 </div>
 
-                <div class="col-sm-6">
-                  <div class="footer-links">
-                    <h4>Useful Links</h4>
-                    <ul>
-                      <li><a href="#">Home</a></li>
-                      <li><a href="#">About us</a></li>
-                      <li><a href="#">Services</a></li>
-                      <li><a href="#">Terms of service</a></li>
-                      <li><a href="#">Privacy policy</a></li>
-                    </ul>
-                  </div>
-
-                  <div class="footer-links">
-                    <h4>Contact Us</h4>
-                    <p>
-                      A108 Adam Street <br>
-                      New York, NY 535022<br>
-                      United States <br>
-                      <strong>Phone:</strong> +1 5589 55488 55<br>
-                      <strong>Email:</strong> info@example.com<br>
-                    </p>
-                  </div>
-
-                  <div class="social-links">
-                    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-                    <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
-                    <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
-                    <a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
-                  </div>
-
+                <div class="footer-newsletter">
+                  <h4>Our Newsletter</h4>
+                  <p>Tenang bersama kami dan miliki waktu lebih untuk keluarga, hobi dan teman Anda!.</p>
+                  <form action="" method="post">
+                    <input type="email" name="email"><input type="submit" value="Subscribe">
+                  </form>
                 </div>
+
+              </div>
+
+              <div class="col-sm-6">
+                <div class="footer-links">
+                  <h4>Useful Links</h4>
+                  <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Terms of service</a></li>
+                    <li><a href="#">Privacy policy</a></li>
+                  </ul>
+                </div>
+
+                <div class="footer-links">
+                  <h4>Contact Us</h4>
+                  <p>
+                    JL.Kartini No 33 Pangarangan <br>
+                    Sumenep, Jawa Timur<br>
+                    Indonesia <br>
+                    <strong>Phone:</strong> +62 8320 3026 3968<br>
+                    <strong>Email:</strong> laundrysehati@gmsil.com<br>
+                  </p>
+                </div>
+
+                <div class="social-links">
+                  <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                  <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                  <a href="https://wa.me/6282330263968" class="whatsapp"><i class="fa fa-whatsapp"></i></a>
+                  <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+                </div>
+
+              </div>
 
             </div>
 
@@ -164,9 +165,8 @@
           <div class="col-lg-6">
 
             <div class="form">
-              
+
               <h4>Send us a message</h4>
-              <p>Eos ipsa est voluptates. Nostrum nam libero ipsa vero. Debitis quasi sit eaque numquam similique commodi harum aut temporibus.</p>
               <form action="" method="post" role="form" class="contactForm">
                 <div class="form-group">
                   <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
@@ -194,7 +194,7 @@
 
           </div>
 
-          
+
 
         </div>
 
@@ -203,7 +203,7 @@
 
     <div class="container">
       <div class="copyright">
-        &copy; Copyright <strong>Rapid</strong>. All Rights Reserved
+        &copy; Copyright <strong>SakeraDev.id</strong>. All Rights Reserved
       </div>
       <div class="credits">
         <!--
@@ -241,4 +241,5 @@
   <script src="{{ asset('/assets/js/main.js') }}"></script>
 
 </body>
+
 </html>

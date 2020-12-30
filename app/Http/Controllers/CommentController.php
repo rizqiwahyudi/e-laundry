8464421e96ec8d771cd5f4e9d539ba48a96621f8
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2020-12-30 21:14:04 +0700

    created PagesController and Renamed HomeController to CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class HomeController extends Controller
+class CommentController extends Controller
 {
     /**
      * Display a listing of the resource.

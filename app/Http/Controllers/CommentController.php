b09cc9ce095d652bb454d951d7d62ded2c910769
rizqiwahyudi <rizqiwahyudi57@gmail.com>
2021-01-10 01:25:48 +0700

    Add sortDesc method in CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -16,7 +16,7 @@ class CommentController extends Controller
      */
     public function index()
     {
-        $comments = Comment::all();
+        $comments = Comment::all()->sortDesc();
         return view('v_index', compact('comments'));
     }
 
